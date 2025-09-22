ba36f0eca63a9c40db96d07113f86c026bc20f4f
AllenTsai1215 <144758485+AllenTsai1215@users.noreply.github.com>
2025-09-22 14:45:51 +0800

    week03

---
+++ week03_1_pinmode_8_OUTPUT__8_800_200/week03_1_pinmode_8_OUTPUT__8_800_200.ino
@@ -0,0 +1,18 @@
+week03_1_pinmode_8_OUTPUT__8_800_200
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(8,OUTPUT);
+  tone(8,800,200);
+  delay(200);
+  pinMode(8,OUTPUT);
+  tone(8,800,200);
+  delay(200);
+  pinMode(8,OUTPUT);
+  tone(8,800,200);
+  delay(200);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
